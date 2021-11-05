540834960e09c2da515f32ef50fe2cf90c53cb91
Chase Covington <mchasecov@gmail.com>
2021-11-05 17:21:19 -0400

    Event Users step 6, 7, 8, 9

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,30 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
+  end
+
+  def new
+    @event =  current_user.created_events.build
+    @user = current_user
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    @user = current_user
+    if @event.save
+      redirect_to events_path(@event.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :event_date, :location, :creator_id)
   end
 end
